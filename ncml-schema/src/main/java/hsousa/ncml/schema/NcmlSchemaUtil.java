380bcf38083890a7be15eaff5bff43ae96989f8e
hlfsousa <49520156+hlfsousa@users.noreply.github.com>
2020-08-29 17:52:32 +0100

    Issue #9: tool to suggest improvements to the NcML header (#34)
    Issue #9: tool to suggest improvements to the NcML header
    
    - Variable filtering
    - Group grouping
    - Recursion into groups, variables first
    - Test for writing transformed header to file

---
+++ ncml-schema/src/main/java/hsousa/ncml/schema/NcmlSchemaUtil.java
@@ -0,0 +1,75 @@
+package hsousa.ncml.schema;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import edu.ucar.unidata.netcdf.ncml.Attribute;
+import edu.ucar.unidata.netcdf.ncml.Group;
+import edu.ucar.unidata.netcdf.ncml.Netcdf;
+
+/**
+ * Utility class for frequent operations on schema classes.
+ * 
+ * @author Henrique Sousa
+ */
+public class NcmlSchemaUtil {
+    
+    private NcmlSchemaUtil() { }
+
+    private static Map<Pattern, String> TYPE_REGEX;
+    
+    static {
+        Map<Pattern, String> typeMapping = new LinkedHashMap<>();
+        typeMapping.put(Pattern.compile("-?\\d+"), "long");
+        typeMapping.put(Pattern.compile("-?(?:\\d+)?\\.\\d+|"), "double");
+        TYPE_REGEX = Collections.unmodifiableMap(typeMapping);
+    }
+
+    /**
+     * Retrieves a map of a selected type of element from a list of child elements. Most child elements, regardless of
+     * type, from {@link Netcdf} and {@link Group} are provided as a list of {@link Object}, so we always have to filter
+     * them. We also usually want the elements by name, so putting them in a map makes location easier.
+     * 
+     * @param <T>        the type of element to be retrieved
+     * @param childList  the list of elements
+     * @param targetType the type of element to be retrieved
+     * @param keyMapper  a function that returns the name of the element
+     * @return a map containing only child elements of the selected type with the name (as per {@code keyMapper}) as key
+     */
+    public static <T> Map<String, T> mapChildren(final List<?> childList, Class<T> targetType, Function<T, String> keyMapper) {
+        @SuppressWarnings("unchecked")
+        Map<String, T> children = childList.stream()
+                .filter(child -> targetType.isInstance(child))
+                .map(child -> (T) child)
+                .collect(Collectors.toMap(keyMapper, child -> child));
+        return children;
+    }
+    
+    public static String inferAttributeType(Attribute attribute) {
+        String type = attribute.getType();
+        if (type == null || type.isEmpty()) {
+            type = inferTypeFromValue(attribute.getValue());
+        }
+        return type;
+    }
+    
+    public static String inferTypeFromValue(String value) {
+        String type = "string";
+        if (value != null) {
+            for (Entry<Pattern, String> mapping : TYPE_REGEX.entrySet()) {
+                if (mapping.getKey().matcher(value).matches()) {
+                    type = mapping.getValue();
+                    break;
+                }
+            }
+        }
+        return type;
+    }
+
+}
---
+++ ncml-schemagen/src/test/java/hsousa/netcdf/schemagen/improvements/HeaderTransformerTest.java
@@ -0,0 +1,161 @@
+package hsousa.netcdf.schemagen.improvements;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static hsousa.ncml.schema.NcmlSchemaUtil.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+
+import org.junit.jupiter.api.Test;
+
+import edu.ucar.unidata.netcdf.ncml.Attribute;
+import edu.ucar.unidata.netcdf.ncml.Dimension;
+import edu.ucar.unidata.netcdf.ncml.Group;
+import edu.ucar.unidata.netcdf.ncml.Netcdf;
+import edu.ucar.unidata.netcdf.ncml.Variable;
+import hsousa.netcdf.schemagen.improvements.filtering.ElementFilter;
+import hsousa.netcdf.schemagen.improvements.filtering.GroupArchetypeFilter;
+import hsousa.netcdf.schemagen.improvements.filtering.VariableAttributeFilter;
+
+public class HeaderTransformerTest {
+
+    private Netcdf schema;
+    private HeaderTransformer transformer;
+
+    public void readNcmlHeader(String headerLocation) throws Exception {
+        URL schemaURL = new File(headerLocation).toURI().toURL();
+        JAXBContext context = JAXBContext.newInstance(Netcdf.class);
+        schema = (Netcdf) context.createUnmarshaller().unmarshal(schemaURL.openStream());
+    }
+    
+    private void writeTransformedHeaderToFile(Netcdf transformedHeader) {
+        try {
+            File tmpFile = File.createTempFile("HeaderTransformerTest", ".xml");
+            tmpFile.deleteOnExit();
+            JAXBContext context = JAXBContext.newInstance(Netcdf.class);
+            context.createMarshaller().marshal(transformedHeader, tmpFile);
+        } catch (IOException | JAXBException e) {
+            fail("Unable to create temporary file", e);
+        }
+    }
+    
+    @Test
+    public void testGroupMergingByCommonDefinition() throws Exception {
+        readNcmlHeader("../ncml-binding-examples/samples/test_hgroups.xml");
+        // variables in test_hgroups.xml use "name" instead of "long_name", so the default transformation does not apply
+        transformer = new HeaderTransformer() {
+            private final ElementFilter<Variable> variableFilter = new VariableAttributeFilter()
+                    .withCommonValue("unit")
+                    .withNameBasedOn("name")
+                    .withSequentialMatching(true);
+            private final ElementFilter<Group> groupFilter = new GroupArchetypeFilter();
+
+            @Override
+            protected List<ElementFilter<Group>> getGroupFilters() {
+                return Arrays.asList(groupFilter);
+            }
+
+            @Override
+            protected List<ElementFilter<Variable>> getVariableFilters() {
+                return Arrays.asList(variableFilter);
+            }
+        };
+
+        Netcdf transformed = transformer.modify(schema);
+        writeTransformedHeaderToFile(transformed);
+        Map<String, Group> groups = mapChildren(transformed.getEnumTypedefOrGroupOrDimension(), Group.class,
+                group -> group.getName().contains(":") ? group.getName().substring(0, group.getName().indexOf(':'))
+                        : group.getName());
+
+        {
+            Group mozaicFlight = groups.get("mozaic_flight_2012030");
+            assertThat(mozaicFlight, is(notNullValue()));
+            String regex = mozaicFlight.getName().substring(mozaicFlight.getName().indexOf(':') + 1);
+            assertThat("mozaic_flight_2012030412545335_ascent", matchesPattern(regex));
+            Map<String, Dimension> dimensions = mapChildren(mozaicFlight.getEnumTypedefOrDimensionOrVariable(), Dimension.class,
+                    dim -> dim.getName());
+            /*
+             * dimension size is not going to be accurate (each group has its own), but the actual value will be set to
+             * and retrieved from the variable interface when reading and writing the file, respectively
+             */
+            assertThat(dimensions.get("air_press"), is(notNullValue()));
+            
+            Map<String, Variable> variables = mapChildren(mozaicFlight.getEnumTypedefOrDimensionOrVariable(), Variable.class,
+                    var -> var.getName());
+            assertThat(variables.keySet(),
+                    is(new HashSet<>(Arrays.asList("air_press", "mole_fraction_of_:CO|O3", "altitude", "UTC_time", "lat", "lon"))));
+
+            Map<String, Attribute> attributes = mapChildren(mozaicFlight.getEnumTypedefOrDimensionOrVariable(), Attribute.class,
+                    att -> att.getName());
+            assertThat(attributes.keySet(), is(new HashSet<>(Arrays.asList("airport_dep", "flight", "level",
+                    "airport_arr", "mission", "time_dep", "aircraft", "link", "phase", "time_arr"))));
+        }
+    }
+    
+    @Test
+    public void testVariableMergingByUnitLongName() throws Exception {
+        readNcmlHeader("../ncml-binding-examples/samples/cami_0000-09-01_64x128_L26_c030918.xml");
+        transformer = new DefaultHeaderTransformer();
+
+        Netcdf transformed = transformer.modify(schema);
+        writeTransformedHeaderToFile(transformed);
+        Map<String, Variable> variables = mapChildren(transformed.getEnumTypedefOrGroupOrDimension(), Variable.class,
+                var -> var.getName());
+        
+        {
+            Variable hybridLevel = variables.get("hybrid_level_at:lev|ilev");
+            assertThat(hybridLevel, is(notNullValue()));
+            assertThat(hybridLevel.getType(), is("double"));
+            assertThat(hybridLevel.getShape().split("\\s++").length, is(1));
+            String[] emptyAttributes = { "long_name", "formula_terms" };
+            Map<String, String> notEmptyAttributes = new LinkedHashMap<>();
+            notEmptyAttributes.put("units", "level");
+            notEmptyAttributes.put("positive", "down");
+            notEmptyAttributes.put("standard_name", "atmosphere_hybrid_sigma_pressure_coordinate");
+            notEmptyAttributes.put("_FillValue", "9.99999961690316e+35");
+            Map<String, Attribute> attributes = mapChildren(hybridLevel.getAttribute(), Attribute.class,
+                    attribute -> attribute.getName());
+            testAttributes(attributes, emptyAttributes, notEmptyAttributes);
+        }
+
+        {
+            Variable wind = variables.get("onal_wind:U|V");
+            assertThat(wind, is(notNullValue()));
+            assertThat(wind.getShape().split("\\s++").length, is(4));
+            assertThat(wind.getType(), is("double"));
+            String[] emptyAttributes = { "long_name" };
+            Map<String, String> notEmptyAttributes = new LinkedHashMap<>();
+            notEmptyAttributes.put("units", "m/s");
+            Map<String, Attribute> attributes = mapChildren(wind.getAttribute(), Attribute.class,
+                    attribute -> attribute.getName());
+            testAttributes(attributes, emptyAttributes, notEmptyAttributes);
+        }
+    }
+
+    private void testAttributes(Map<String, Attribute> attributes, String[] emptyAttributes,
+            Map<String, String> notEmptyAttributes) {
+        for (String attributeName : emptyAttributes) {
+            Attribute attribute = attributes.get(attributeName);
+            assertThat(attribute.getValue(), is(emptyOrNullString()));
+            assertThat(attribute.getType(), is(equalToIgnoringCase("string")));
+        }
+        
+        for (Entry<String, String> notEmptyEntry : notEmptyAttributes.entrySet()) {
+            Attribute attribute = attributes.get(notEmptyEntry.getKey());
+            assertThat(attribute.getValue(), is(notEmptyEntry.getValue()));
+        }
+    }
+
+}
